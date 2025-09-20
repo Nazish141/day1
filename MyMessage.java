7df8df1dcc9a78d7bc3daf09ca812cf562c249fe
Nazish <nazishnoreen01@email.com>
2025-09-20 17:22:33 +0500

    DIY2

---
+++ MyMessage.java
@@ -0,0 +1,9 @@
+public class MyMessage {
+    public static void main(String[] args) {
+        System.out.println("i want to learn it because coding helps develop");
+        System.out.print("it's problem solving skill");
+        System.out.println("it turns ideas into reality");
+        System.out.print("i learn it to create amazing things");
+    }
+}
+        
